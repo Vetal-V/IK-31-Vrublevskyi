f054fce968c2825ccbdc7379a256a24897ff3b5a
Vetal-V <54945817+Vetal-V@users.noreply.github.com>
2019-10-21 20:43:09 +0300

    Lab_3: Add tasks of lab work

---
+++ lab_3/monitoring.py
@@ -0,0 +1,36 @@
+import requests
+import json
+import logging
+from requests.exceptions import HTTPError
+import time
+
+logging.basicConfig(
+    filename="server.log",
+    filemode='a',
+    level=logging.INFO,
+    format='{levelname} {asctime} {name} : {message}',
+    style='{'
+)
+log = logging.getLogger(__name__)
+
+
+def main(url):
+    try:
+        r = requests.get(url)
+        r.raise_for_status()
+    except HTTPError as http_err:
+        logging.warning("Сервер недоступний. Будь ласка, перезапустіть сервер ще раз. Помилка: %s", http_err)
+    except Exception as err:
+        logging.error("Сервер недоступний. Будь ласка, запустіть сервер ще раз. Помилка: %s", err)
+    else:
+        data = json.loads(r.content)
+        logging.info("Сервер доступний. Час на сервері: %s", data['date'])
+        logging.info("Запитувана сторінка: : %s", data['current_page'])
+        logging.info("Відповідь сервера місти наступні поля:")
+        for key in data.keys():
+            logging.info("Ключ: %s, Значення: %s", key, data[key])
+
+if __name__ == '__main__':
+    while True:
+        main("http://localhost:8000/health")
+        time.sleep(60)
