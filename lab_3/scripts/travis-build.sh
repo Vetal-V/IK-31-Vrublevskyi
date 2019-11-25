48d5ccc3256d198f6b311f902e6662a5ea3427a7
Vetal-V <54945817+Vetal-V@users.noreply.github.com>
2019-11-25 19:45:51 +0200

    Lab_6: Update scripts

--- lab_3/scripts/travis-build.sh
+++ lab_3/scripts/travis-build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 set -ev
 nohup pipenv run server > ./ci-build.log &
-pipenv run python monitoring.py || true
+pipenv run python monitoring.py || true &
+sleep 60
+kill `jobs -p`
 exit 0
